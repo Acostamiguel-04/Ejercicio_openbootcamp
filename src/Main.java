886556f38732a34197442a7b39a8944e4f943ffd
Miguel <acoacostamiguel@gmail.com>
2023-05-14 17:38:34 -0300

    tp terminado

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,41 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hola Mundo");
+           int resultado =0;
+          resultado=suma(10, 20,30);
+          System.out.println("La cantidad de la suma es: " +resultado);
+
+
+
+        Auto miAuto= new Auto();
+        miAuto.sumarpuertas();
+
+        System.out.println("La cantidad de las puerta son: " +miAuto.puertas);
+        }
+
+
+
+
+        public static int suma (int a, int b, int c)
+        {
+            return a+b+c;
+
+
+
+
+
 
         }
+
+
+
+        static class Auto {
+            public int puertas=4;
+
+            public void sumarpuertas(){
+                this.puertas++;
+            }
+
+        }
+
     }
